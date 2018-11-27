4c5a6f8e538cc657b61de233d60236dce5858c97
AlvaroFA <fragaarnaizalvaro@gmail.com>
2018-11-27 12:23:20 +0100

    updated files
    Added some new methods

--- JS/Practices/tarea3Dwec/js/mates.js
+++ JS/Practices/tarea3Dwec/js/mates.js
@@ -1,9 +1,29 @@
+//Área del círculo: pedirá al usuario el radio en cm (con un prompt) y devolverá el área de la circunferencia.
 function calcularAreaCirculo(){
+
+    var prompt= window.prompt("Introduzca valor de radio en cm");
+    var radio= parseFloat(prompt);
+    var resultado=Math.PI*Math.pow(radio,2);
+    document.getElementById('area').innerHTML='El area del circulo es '+resultado +' cm' ;
     
 }
-
+//Trigonometría: al pulsar, pedirá al usuario un ángulo en grados (entre 0 y 360 mediante un prompt) y devolverá los valores del seno, coseno y tangente (en un alert).
 function trigonomiaAngulo(){
+    var prompt= window.prompt("Introduzca el angulo ,en grados,a calcular ");
+    var grados = parseInt(prompt);
+    var radianes= grados* Math.PI/180;
 
+
+    //Calculos trigonometricos en base a radianes
+    var resultado='Los resultados de '+ grados + ' es:'+ '\n';
+    var coseno= Math.cos(radianes);
+    resultado+= 'cos:'+coseno+'\n';
+    var seno= Math.sin(radianes);
+    resultado+= seno+'\n';
+    var tangente= Math.tan(radianes);
+    resultado+= tangente+'\n';
+    alert(resultado);
 }
+
 
 
--- JS/Practices/tarea3Dwec/js/ventana.js
+++ JS/Practices/tarea3Dwec/js/ventana.js
@@ -1,7 +1,74 @@
+var ventana=null
 function abrirVentana() {
-    var ventana = null;
-    if (ventana == !null){
-ventana = window.open("html/ventana.html", "Alvaro Fraga Arnaiz");
+
+    //Se abrirá una nueva ventana que cargue el código de ventana.html.
+    // Verificar previamente si existe la ventana: si no exite se abre y si existe, se muestra.
+    // La ventena tendrá que incluir como título tu nombre y no mostrará barra de navegación.
+    if  (ventana== null || ventana.closed)
+    {
+
+        ventana  =  window.open("html/ventana.html",  "Alvaro Fraga Arnaiz", "width=420,height=230,toolbar=0,menubar=0");
+        ventana.focus();
+    }else{
+
+        alert('Ventana ya abierta');
+    }
+
 }
-    alert('La ventana esta ya abierta');
+
+
+function cerrarVentana() {
+
+    if  (ventana != null)
+    {
+        ventana.close();
+        ventana=null;
+
+    }else{
+        alert('Ventana ya cerrada');
+    }
+
+
 }
+
+//Mover la ventana a la posición 100x100px.
+// Verificar previamente si la ventana existe: si no existiese se mostraría un error mediante alert.
+function moverVentana(){
+
+    if  (ventana != null)
+    {
+        ventana.moveTo(100,100);
+        ventana=null;
+
+    }else{
+        alert('Ventana no abierta');
+    }
+}
+//Redimensionar: Crecer el tamaño de la ventana en 10px en ambas medidas.
+// Verificar previamente si la ventana existe: si no existiese se mostraría un error mediante alert.
+function redimensionarVentana(){
+
+    if  (ventana != null)
+    {
+        ventana.focus();
+        ventana.resizeBy(10,10);
+    }else{
+        alert('Ventana no abierta');
+    }
+}
+
+//Desplazar: moverá la ventanta hacia la derecha cha 10px y abajo 20px.
+// Verificar previamente si la ventana existe: si no existiese se mostraría un error mediante alert.
+
+function desplazarVentana(){
+
+    if  (ventana != null)
+    {
+        ventana.focus();
+        ventana.moveBy(10,20);
+    }else{
+        alert('Ventana no abierta');
+    }
+}
+
+
